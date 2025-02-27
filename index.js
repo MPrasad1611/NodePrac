a908ca2bdc555e2d1715dab4f84e6ca6191087cc
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-27 13:56:59 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const app = require("http");
+const url=require("url")
 // const server=app.createServer((req,res)=>{
 //     res.write("hello world")
 //     res.end("response Provided")
 // })
 let obj = { name: "Prasad", city: "vizag" };
 const server = app.createServer((req, res) => {
+  const purl=url.parse(req.url,true)
+  console.log(purl.query.tickets)
   if (req.method == "GET") {
     if (req.url == "/home") {
       res.writeHead(201, "responsive received", {
