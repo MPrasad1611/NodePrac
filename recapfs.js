876007610819232d8cc7dfba6a61dc7879efc0d2
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-03-02 12:08:31 +0530

    changes

--- recapfs.js
+++ recapfs.js
@@ -31,7 +31,29 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.method == "GET") {
-    res.end("GET request received"); 
+     fs.readFile("./data.json","utf8", (err, data) => {
+              if (err) {
+                  res.write(err);
+                  res.end();
+              }else {
+                 res.writeHead(200, { "Content-Type": "application/json" });
+                //  console.log(JSON.parse(data));
+                //   res.write(data);
+                //   res.end();
+                   let existingData = JSON.parse(data);
+                   existingData.push(6)
+                   console.log(existingData);
+                   fs.writeFile("./data.json", JSON.stringify(existingData), (err) => {
+                    if (err) {
+                      res.write(err);
+                      res.end();
+                    } else {
+                      res.write("Data saved successfully");
+                      res.end();
+                    }
+                   })
+              }
+     });
   } 
   else if (req.method == "POST") {
     let ipData = "";
