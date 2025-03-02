3d46b3cddeb7f20501723425d3c905317eb73695
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-03-02 12:25:21 +0530

    changes

--- recapfs.js
+++ recapfs.js
@@ -2,8 +2,8 @@
 // const fs = require("fs");
 // const server=http.createServer((req,res)=>{
 //   if(req.method=="GET"){
-     
-//   } 
+
+//   }
 //   else if(req.method=="POST"){
 //     let ipData = "";
 //       req.on("data", (chunk) => {
@@ -14,7 +14,7 @@
 //           console.log(err);
 //         }
 //         else{
-           
+
 //           res.end("Data saved successfully")
 //         }
 //       })
@@ -31,38 +31,37 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.method == "GET") {
-     fs.readFile("./data.json","utf8", (err, data) => {
-              if (err) {
-                  res.write(err);
-                  res.end();
-              }else {
-                 res.writeHead(200, { "Content-Type": "application/json" });
-                //  console.log(JSON.parse(data));
-                //   res.write(data);
-                //   res.end();
-                   let existingData = JSON.parse(data);
-                   existingData.push(6)
-                   console.log(existingData);
-                   fs.writeFile("./data.json", JSON.stringify(existingData), (err) => {
-                    if (err) {
-                      res.write(err);
-                      res.end();
-                    } else {
-                      res.write("Data saved successfully");
-                      res.end();
-                    }
-                   })
-              }
-     });
-  } 
-  else if (req.method == "POST") {
+    fs.readFile("./data.json", "utf8", (err, data) => {
+      if (err) {
+        res.write(err);
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        //  console.log(JSON.parse(data));
+        //   res.write(data);
+        //   res.end();
+        let existingData = JSON.parse(data);
+        existingData.push(6);
+        console.log(existingData);
+        fs.writeFile("./data.json", JSON.stringify(existingData), (err) => {
+          if (err) {
+            res.write(err);
+            res.end();
+          } else {
+            res.write("Data saved successfully");
+            res.end();
+          }
+        });
+      }
+    });
+  } else if (req.method == "POST") {
     // let ipData = "";
 
     // req.on("data", (chunk) => {
     //   ipData += chunk;
     // });
 
-    // req.on("end", () => { 
+    // req.on("end", () => {
     //   fs.writeFile("./info.txt", ipData, (err) => {
     //     if (err) {
     //       console.log(err);
@@ -77,25 +76,22 @@ const server = http.createServer((req, res) => {
       if (err) {
         res.write(err);
         res.end();
-      } 
-      else {
-          let newName="Ram"
-          let existingData1=JSON.parse(data)
-          existingData1.push(newName)
-          fs.writeFile("./data.json", JSON.stringify(existingData1), (err) => {
-            if (err) {
-              res.write(err);
-              res.end();
-            } else
-             {
-              res.write("Data saved successfully");
-              res.end();
-             }
-          })
+      } else {
+        let newName = "Ram";
+        let existingData1 = JSON.parse(data);
+        existingData1.push(newName);
+        fs.writeFile("./data.json", JSON.stringify(existingData1), (err) => {
+          if (err) {
+            res.write(err);
+            res.end();
+          } else {
+            res.write("Data saved successfully");
+            res.end();
+          }
+        });
       }
-    }) 
-} 
-  else {
+    });
+  } else {
     res.end("Invalid request");
   }
 });
