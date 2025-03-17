3954db7ef217883c44668521a676c8fefe6a21c4
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-03-17 20:08:15 +0530

    express

--- express4.js
+++ express4.js
@@ -1,18 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
+const fs = require("fs").promises;
+app.use(express.text());
 app.use(express.json());
-app.get('/api', (req, res) => {
-    fs.readFile('./hello.json', 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(JSON.parse(data));
-        }
-    })
-    
-})
+// app.get('/api', (req, res) => {
+// //     fs.readFile('./hello.json', 'utf8', (err, data) => {
+// //         if (err) {
+// //             console.log(err);
+// //         } else {
+// //             res.send(JSON.parse(data));
+// //         }
+// //     })
 
-app.listen(3900, () => {
-    console.log("server is running on port 3900")
+// })
+// app.post('/api', (req, res) => {
+
+//     // let indata ="nodejs,express.js,mongodb"
+//     let indata = req.body;
+//     console.log(indata);
+//     fs.appendFile('./new.json', JSON.stringify(indata), (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("data written to file");
+//             res.send("data written to file");
+//         }
+//     });
+//     });
+
+app.post("/api", async (req, res) => {
+  try {
+    let indata = req.body;
+    let edata = JSON.parse(await fs.readFile("./new.json", "utf8"));
+    let iseal = edata.find((x, y) => {
+      return x.id == indata.id;
+    });
+    console.log(iseal);
+    if (iseal) {
+      res.send("id already exist");
+    } else {
+      edata.push(indata);
+      let udata = edata;
+      await fs.writeFile("./new.json", JSON.stringify(udata));
+      res.send("data written to file");
+    }
+  } catch (err) {
+    res.send(err);
+    console.log(err);
+  }
+});
+app.delete("/fun", async (req, res) => {
+   try {
+    await fs.unlink("./hello1.txt")
+    res.send("file deleted")
+   }catch (err) {
+    console.log(err);
+   }
 })
+app.listen(3900, () => {
+  console.log("server is running on port 3900");
+});
